a4060fe13fba3f0ee6dbb99f170b8651af030d26
jdesk <jamesbarlo@gmail.com>
2018-11-01 15:30:19 -0400

    saving changes to net scripts

--- scripts/full-node-mainnet-start.sh
+++ scripts/full-node-mainnet-start.sh
@@ -2,7 +2,7 @@
 set -u
 set -e
 
-GLOBAL_ARGS="--rpc --rpcaddr 0.0.0.0 --rpcport 9565 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --emitcheckpoints --ws --wsport 9555 --wsaddr 0.0.0.0 --wsorigins" * --wsapi eth,web3"
+GLOBAL_ARGS="--rpc --rpcaddr 0.0.0.0 --rpcport 9565 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --emitcheckpoints --ws --wsport 9555 --wsaddr 0.0.0.0 --wsorigins 0.0.0.0 --wsapi eth,web3 --rpcvhosts rpctest.haloplatform.tech"
 
 echo "[*] Starting full node"
-geth --syncmode full --networkid 42568378638 --datadir /root/.go-haloplatform/qdata $GLOBAL_ARGS --port 50607 --ipcpath /root/.go-haloplatform/qdata/geth.ipc
+geth --syncmode full --networkid 42568378638 --datadir /go-halo/qdata $GLOBAL_ARGS --port 50607 --ipcpath /go-halo/qdata/geth.ipc
